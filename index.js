d0a6e42a0eb92267b8160ccb55556c5737f4d9af
ayushchauhan1 <ayush_c@ar.iitr.ac.in>
2022-06-08 05:11:20 +0530

    cors added

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const bodyparser = require("body-parser");
 const login = require("./routes/login");
 const cheaque = require("./routes/cheaque");
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 mongoose.connect(
   process.env.DB_CONNECTION,
   { useNewUrlParser: true, useUnifiedTopology: true },
